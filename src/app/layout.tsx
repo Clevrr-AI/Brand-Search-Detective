3e076a7c11a23e517d3387c9c2e0cadbd4524e70
Yuvraj <yuvraj.dagur928@gmail.com>
2025-09-01 15:24:53 +0530

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/next';
 import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${poppins.className} antialiased h-full`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
